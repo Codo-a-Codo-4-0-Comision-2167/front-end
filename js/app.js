f7afd12aa8f3014f82a7895d208c5aec1d8bd044
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-09-20 21:14:21 -0300

    javascript code

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log("Arranca la pagina... ahora desde un archivo js");
+
+// o let
+var soyUnaVariable = " Un string con comillas 'Esto es un string' " ;
+
+var soyUnNumeroEntero = -123131313;
+
+var soyUnNumeroFlotante = 23.54;
+
+const ESTO_ES_UNA_CONSTANTE = "Soy constante...";
+
+const PI = 3.141528;
+
+console.log(ESTO_ES_UNA_CONSTANTE + PI); // Usar log en la consola
+console.log(PI)
+console.dir(document.body); // usar dir en la consola...
